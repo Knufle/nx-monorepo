d3d274cb5303ab0f070ea284bd8c0bb647350eb6
Knufle <iamknufle@gmail.com>
2024-08-29 19:17:18 -0300

    Rename nx-monorepo to frontend

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -3,7 +3,7 @@ import NxWelcome from './nx-welcome';
 export function App() {
   return (
     <div>
-      <NxWelcome title="nx-monorepo" />
+      <NxWelcome title="frontend" />
     </div>
   );
 }
